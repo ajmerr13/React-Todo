4153918d5b7392471eeb6d6c642cc9528924d3cc
ajmerr13 <oliverclosoff07@gmail.com>
2019-12-02 19:10:32 -0500

    mvp

--- src/App.js
+++ src/App.js
@@ -1,13 +1,93 @@
 import React from 'react';
+import TodoList from './components/TodoComponents/TodoList';
+import TodoForm from './components/TodoComponents/TodoForm';
+
+
+const data = [
+  {
+    name: "Feed Dog",
+    id: 1,
+    finished: false
+  },
+  {
+    name: "Wash Dishes",
+    id: 2,
+    finished: false
+  },
+  {
+    name: "Do Laundry",
+    id: 3,
+    finished: false
+  },
+  {
+    name: "Make The Bed",
+    id: 4,
+    finished: false
+  },
+  {
+    name: "Buy Groceries",
+    id: 5,
+    finished: false
+  },
+];
 
 class App extends React.Component {
-  // you will need a place to store your state in this component.
-  // design `App` to be the parent component of your application.
-  // this component is going to take care of state, and any change handlers you need to work with your state
+  constructor(){
+    super();
+    this.state = {
+      chores: data
+    }
+    this.toggleFinished = this.toggleFinished.bind(this);
+  }
+
+  toggleFinished(choreId) {
+    console.log('toggleFinished', choreId);
+
+    this.setState({
+      chores: this.state.chores.map(chore => {
+        if(chore.id === choreId){
+          return {
+            ...chore,
+            finished: !chore.finished
+          };
+        }
+        return chore;
+      })
+    })
+  }
+
+  clearChore = () => {
+    console.log('clearchore');
+    this.setState({
+      chores: this.state.chores.filter(chore => {
+        return !chore.finished;
+      })
+    })
+  }
+
+  addChore = (choreName) => {
+    console.log('addChore: ', choreName);
+    this.setState({
+      chores: [
+        ...this.state.chores,
+        {
+          name: choreName,
+          id: Date.now(),
+          finished: false
+        }
+      ]
+    })
+  }
   render() {
     return (
       <div>
         <h2>Welcome to your Todo App!</h2>
+        <TodoForm addChore={this.addChore} />
+        <TodoList 
+         chores={this.state.chores}
+         toggleFinished={this.toggleFinished}
+         clearChore={this.clearChore}
+         />
       </div>
     );
   }
---
+++ src/components/TodoComponents/TodoForm.js
@@ -0,0 +1,44 @@
+import React from "react";
+import './Todo.css';
+
+class TodoForm extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      choreName: ""
+    };
+  }
+
+  handleChanges = e => {
+      this.setState({
+          choreName: e.target.value
+      })
+  }
+
+  handleSubmit = e => {
+      e.preventDefault();
+      if (this.state.choreName !== ''){
+          this.props.addChore(this.state.choreName);
+          this.setState({
+              choreName: ''
+          });
+      }
+  }
+
+  render(){
+      return (
+          <form onSubmit={this.handleSubmit}>
+              <input 
+                onChange={this.handleChanges}
+                type='text'
+                name='chore'
+                value={this.state.choreName}
+              />
+              <button className='addBtn'>Add</button>
+              <div />
+          </form>
+      )
+  }
+}
+
+export default TodoForm;
